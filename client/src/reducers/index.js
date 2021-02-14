c8cd4a90a8bf58b006c7e5b0f2b93dc7ccc9922a
Olcay Kaplan <olcay.kaplan@datassist.com.tr>
2021-02-14 15:59:20 +0300

    added action reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from "redux";
 import words from './words.js';
+import auth from './auth.js'
 
-export default combineReducers({words});
+export default combineReducers({words, auth});
